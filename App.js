b742264393ea3a4f23c1ffdda4ae1d999458aaec
Yura <y.komarnytskyy@gmail.com>
2023-01-18 16:10:50 +0200

    base context presets

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
 import ShowScreen from "./src/screens/ShowScreen";
 import CreateScreen from "./src/screens/CreateScreen";
 import EditScreen from "./src/screens/EditScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
   {
@@ -20,4 +22,12 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
